94ac09e2dc2f983cc24afcb8841de82be5e0bf1e
Nghia Truong <tmnghia1992@gmail.com>
2021-08-04 10:49:13 +0700

    adding client socket connection

--- client/devmanc/devmanc.c
+++ client/devmanc/devmanc.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 #include "devmanc.h"
 
@@ -38,7 +41,8 @@ void removeNewline(char * str)
 
 DEVMAN_RET main()
 {
-    clientInfo *info = NULL;
+    clientInfo *info    = NULL;
+    int sockfd          = -1;
 
     info = (clientInfo *)calloc(1, sizeof(clientInfo));
     if (info == NULL) {
@@ -55,7 +59,50 @@ DEVMAN_RET main()
     printf("Device CPU: %s\n", info->cpu);
     printf("Device Memory: %s\n", info->memory);
 
+    if (initSocket(&sockfd) != DEVMAN_SUCCESS) {
+        fprintf(stderr, "Error when initialzing socket\n");
+        return DEVMAN_FAILURE;
+    }
+
+    int sent = send(sockfd, info, sizeof(clientInfo), 0);
+    printf("sent %d bytes\n", sent);
+
+    // while(1) {
+    // }
+
     free(info);
+    info = NULL;
+
+    return DEVMAN_SUCCESS;
+}
+
+DEVMAN_RET initSocket(int *sockfd)
+{
+    struct sockaddr_in serv_addr;
+    int rc;
+
+    *sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (*sockfd < 0) {
+        fprintf(stderr, "Error when creating socket\n");
+        return DEVMAN_FAILURE;
+    }
+    printf("Created socket: %d\n", *sockfd);
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(DEVMAN_SERVER_PORT);
+    // Convert the addresses from text to binary form
+    if (inet_pton(AF_INET, DEVMAN_SERVER_ADDRESS, &serv_addr.sin_addr) < 1) {
+        fprintf(stderr, "Invalid address: %s\n", DEVMAN_SERVER_ADDRESS);
+        return DEVMAN_FAILURE;
+    }
+
+    rc = connect(*sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    if (rc < 0)
+    {
+        // fprintf(stderr, "Error when connecting to the server: %s\n", DEVMAN_SERVER_ADDRESS);
+        perror("Error when connecting to the server");
+        return DEVMAN_FAILURE;
+    }
 
     return DEVMAN_SUCCESS;
 }
@@ -107,6 +154,8 @@ DEVMAN_RET getName(char *user)
     // Removing leading whitespaces if any
     removeLeadingSpaces(user);
 
+    pclose(shell);
+
     return DEVMAN_SUCCESS;
 }
 
@@ -133,6 +182,8 @@ DEVMAN_RET getCPU(char *cpu)
     // Removing leading whitespaces if any
     removeLeadingSpaces(cpu);
 
+    pclose(shell);
+
     return DEVMAN_SUCCESS;
 }
 
@@ -158,6 +209,8 @@ DEVMAN_RET getMemory(char *memory)
 
     // Removing leading whitespaces if any
     removeLeadingSpaces(memory);
+
+    pclose(shell);
 
     return DEVMAN_SUCCESS;
 }
